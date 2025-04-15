ce028e5fd9f996f3737fa6d6b6cd9414c4a4cba8
LeoDurillon <durillon.leo@outlook.com>
2025-04-15 14:36:30 +0200

    refactor: remove napi from lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,4 @@
-use checker::input_correctly_close;
-use napi_derive::napi;
-use statement::Statement;
-
-
 pub mod statement;
 pub mod checker;
-
-#[napi]
-fn expand(input:String) -> Option<String>{
-  if !input_correctly_close(&input){
-    return None
-  }
-  Some(Statement::new(&input, 0).parse())
-}
 
 
