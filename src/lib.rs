ab3992aa4189b84c01cf5f5cc20da6e9bee95565
LeoDurillon <durillon.leo@outlook.com>
2025-04-15 12:07:38 +0200

    refactor: test

--- src/lib.rs
+++ src/lib.rs
@@ -1,165 +1,17 @@
+use checker::input_correctly_close;
 use napi_derive::napi;
 use statement::Statement;
 
 
 pub mod statement;
+pub mod checker;
 
 #[napi]
-fn expand(input:String) -> String{
-  Statement::new(&input, 0).parse()
+fn expand(input:String) -> Option<String>{
+  if !input_correctly_close(&input){
+    return None
+  }
+  Some(Statement::new(&input, 0).parse())
 }
 
-//Function for test purpose
-pub fn start(input:&str) -> String{
-  Statement::new(&input, 0).parse()
-}
 
-
-#[cfg(test)]
-mod test {
-    use crate::start;
-  #[test]
-  fn bug_test(){
-    assert_eq!("<div>\n\t<p></p>\n</div>\n<p></p>".to_string(),start("(div>p)+p"))
-  }
-
-
-  #[test]
-  fn should_return_html(){
-    assert_eq!("<html></html>".to_string(),start("html"))
-  }
-
-  #[test]
-  fn should_combine_tag(){
-    assert_eq!("<html>\n\t<p></p>\n</html>".to_string(),start("html>p"))
-  }
-
-  #[test]
-  fn can_be_only_siblings(){
-    assert_eq!("<p></p>\n<div></div>".to_string(),start("p+div"));
-    assert_eq!("<p></p>\n<div></div>\n<p></p>".to_string(),start("p+div+p"))
-  }
-
-
-  #[test]
-  fn can_have_multiple_child(){
-    assert_eq!("<html>\n\t<p></p>\n\t<a href=${1}></a>\n</html>".to_string(),start("html>p+a"))
-  }
-
-  #[test]
-  fn child_can_have_child(){
-    let expected = "<html>\n\t<div>\n\t\t<p></p>\n\t\t<div>\n\t\t\t<p></p>\n\t\t</div>\n\t</div>\n</html>".to_string();
-
-    assert_eq!(expected,start("html>div>p+div>p"))
-  }
-
-  #[test]
-  fn can_handle_tag_group(){
-    let expected = "<html>\n\t<div>\n\t\t<p></p>\n\t</div>\n\t<div>\n\t\t<p></p>\n\t</div>\n</html>".to_string();
-    assert_eq!(expected,start("html>(div>p)+div>p"));
-  
-    let expected = "<html>\n\t<div>\n\t\t<div>\n\t\t\t<p></p>\n\t\t</div>\n\t\t<div>\n\t\t\t<p></p>\n\t\t</div>\n\t</div>\n\t<div>\n\t\t<p></p>\n\t</div>\n</html>".to_string();
-    assert_eq!(expected,start("html>(div>(div>p)+div>p)+div>p"));
-
-    let expected = "<html>\n\t<div>\n\t\t<p></p>\n\t</div>\n\t<div>\n\t\t<div>\n\t\t\t<p></p>\n\t\t</div>\n\t\t<p></p>\n\t</div>\n</html>".to_string();
-    assert_eq!(expected,start("html>(div>p)+div>(div>p)+p"))
-  }
-
-  #[test]
-  fn element_can_multiply(){
-    let expected = "<p></p>\n<p></p>\n<p></p>";
-    assert_eq!(expected.to_string(),start("p*3"));
-
-    let expected = "<div>\n\t<p></p>\n\t<p></p>\n\t<p></p>\n</div>";
-    assert_eq!(expected.to_string(),start("div>p*3"))
-  }
-
-  #[test]
-  fn groups_can_multiply(){
-    let expected = "<html>\n\t<div>\n\t\t<p></p>\n\t</div>\n\t<div>\n\t\t<p></p>\n\t</div>\n\t<div>\n\t\t<p></p>\n\t</div>\n</html>";
-    assert_eq!(expected.to_string(),start("html>(div>p)*3"))
-  }
-
-  #[test]
-  fn element_can_be_single() {
-    let expected = "<html>\n\t<Icon/>\n</html>";
-    assert_eq!(expected.to_string(),start("html>Icon/"))
-  }
-
-  #[test]
-  fn element_cant_be_single_if_has_child(){
-    let expected = "<html>\n\t<Icon>\n\t\t<p></p>\n\t</Icon>\n</html>";
-    assert_eq!(expected.to_string(),start("html>Icon/>p"))
-  }
-
-  #[test]
-  fn element_can_have_class() {
-    let expected = "<div class=\"test echo bravo\"></div>";
-    assert_eq!(expected.to_string(),start("div.test.echo.bravo"))
-  }
-  #[test]
-  fn single_element_can_have_class() {
-    let expected = "<div class=\"test echo bravo\"/>";
-    assert_eq!(expected.to_string(),start("div.test.echo.bravo/"))
-  }
-
-
-  #[test]
-  fn element_can_have_props() {
-    let expected = "<Table header={title} name=\"my-table\"></Table>";
-    assert_eq!(expected.to_string(),start("Table:header={title}:name=my-table"))
-  }
-
-  #[test]
-  fn props_can_have_dot() {
-    let expected = "<Table header={table.title} name=\"my-table\"></Table>";
-    assert_eq!(expected.to_string(),start("Table:header={table.title}:name=my-table"))
-  }
-
-  #[test]
-  fn element_can_have_destructured_props() {
-    let expected = "<Table {...props}></Table>";
-    assert_eq!(expected.to_string(),start("Table:{...props}"))
-  }
-
-  #[test]
-  fn single_element_can_have_props() {
-    let expected = "<img src={image} alt=\"my own image\"/>";
-    assert_eq!(expected.to_string(),start("img:src={image}:alt=my own image/"))
-  }
-
-  #[test]
-  fn element_can_have_text() {
-    let expected = "<div>My test text is awesome</div>";
-    assert_eq!(expected.to_string(),start("div<My test text is awesome"))
-  }
-
-  #[test]
-  fn element_can_have_all() {
-    let expected = "<div class=\"test\" data={myData}>My test text is awesome</div>";
-    assert_eq!(expected.to_string(),start("div.test:data={myData}<My test text is awesome"))
-  }
-
-  #[test]
-  fn should_handle_complex_query(){
-    let expected = "<div>\n\t<div class=\"test\">\n\t\t<p>My text</p>\n\t\t<Table header={header}></Table>\n\t</div>\n\t<footer>My footer</footer>\n</div>";
-
-    assert_eq!(expected.to_string(),start("div>(div.test>p<My text+Table:header={header})+footer<My footer"));
-    
-    let expected = "<div class=\"fixed bottom-0 left-0 right-0 top-0 z-20 flex animate-appear flex-col items-center justify-center bg-black/70 backdrop-blur-md\" onClick$={onHide$}>\n\t<div class=\"flex flex-col items-center justify-center p-20\">\n\t\t<div class=\"delay animate-appear opacity-0\">\n\t\t\t<Logo class=\"size-40\" fill=\"white\"/>\n\t\t</div>\n\t\t<div>\n\t\t\t<Form class=\"mx-2 flex animate-slideB flex-col items-center gap-2 rounded-lg bg-surface p-5\" action={login}>\n\t\t\t\t<div class=\"flex items-center gap-4\">\n\t\t\t\t\t<p class=\"text-3xl font-medium\">Connexion</p>\n\t\t\t\t\t<Icon icon=\"lock\"/>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"flex flex-col gap-2 self-stretch px-3\">\n\t\t\t\t\t<Textfield label=\"Email\" type=\"email\" required name=\"email\" placeholder=\"j.dupont@example.com\" icon=\"atSign\"/>\n\t\t\t\t\t<Textfield label=\"Password\" type=\"password\" required name=\"password\" placeholder=\"Votre mot de passe\" icon=\"lock\"/>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"flex flex-col gap-2\">\n\t\t\t\t\t<Button color=\"current\" type=\"submit\">Connexion</Button>\n\t\t\t\t\t<Link href=\"#\" class=\"text-secondary transition-colors\">Mot de passe oublié ?</Link>\n\t\t\t\t</div>\n\t\t\t</Form>\n\t\t</div>\n\t</div>\n</div>";
-    assert_eq!(expected.to_string(),start("div.fixed.bottom-0.left-0.right-0.top-0.z-20.flex.animate-appear.flex-col.items-center.justify-center.bg-black/70.backdrop-blur-md:onClick$={onHide$}>div.flex.flex-col.items-center.justify-center.p-20>(div.delay.animate-appear.opacity-0>Logo.size-40:fill=white/)+div>Form.mx-2.flex.animate-slideB.flex-col.items-center.gap-2.rounded-lg.bg-surface.p-5:action={login}>(div.flex.items-center.gap-4>p.text-3xl.font-medium<Connexion+Icon:icon=lock/)+(div.flex.flex-col.gap-2.self-stretch.px-3>Textfield:label=Email:type=email:required;:name=email:placeholder=j.dupont@example.com:icon=atSign/+Textfield:label=Password:type=password:required;:name=password:placeholder=Votre mot de passe:icon=lock/)+div.flex.flex-col.gap-2>Button:color=current:type=submit<Connexion+Link:href=#.text-secondary.transition-colors<Mot de passe oublié ?"))
-  }
-
-  #[test]
-  fn should_handle_snippet(){
-    let expected = "<a href=${1}></a>";
-    assert_eq!(expected.to_string(),start("a"));
-
-    let expected = "<>\n\t<label for=${1}></label>\n\t<input type=\"checkbox\" name=${1} id=${1}/>\n</>";
-    assert_eq!(expected.to_string(),start("e>label+input:c"));
-    assert_eq!(expected.to_string(),start(">label+input:c"));
-
-    let expected = "<div class=\"foo bar\"></div>";
-    assert_eq!(expected.to_string(),start(".foo.bar"));
-  }
-}
--- src/statement/mod.rs
+++ src/statement/mod.rs
@@ -45,7 +45,7 @@ impl Statement {
     let first_opening =input.split_at(input.find("(").unwrap_or(input.len()));
     let mut multiplier = 1;
   
-    let mut order = [first_down.0.len(),first_sibling.0.len(),first_opening.0.len()];
+    let mut order = [first_down.0.len(),first_sibling.0.len(),if first_opening.0.len() == 0 {first_opening.0.len()} else {usize::MAX}];
     order.sort();
     
     let first = order.get(0).unwrap();
@@ -141,48 +141,166 @@ impl Statement {
 
 
 
-
-
 #[cfg(test)]
-mod test {
-    use crate::statement::Statement;
-
-  #[test]
-  fn test_parser() {
-    let expected = (("html","",1),"");
-    assert_eq!(expected,Statement::parse_input("html"));
-
-    let expected = (("html","",3),"");
-    assert_eq!(expected,Statement::parse_input("html*3"));
-
-    let expected = (("html","p",1),"");
-    assert_eq!(expected,Statement::parse_input("html>p"));
-
-    let expected = (("html","",1),"p");
-    assert_eq!(expected,Statement::parse_input("html+p"));
-
-    let expected = (("html","",1),"div>p+icon");
-    assert_eq!(expected,Statement::parse_input("html+div>p+icon"));
-
-    let expected = (("html","div>p+icon",1),"");
-    assert_eq!(expected,Statement::parse_input("html>div>p+icon"));
-
-    let expected = (("html","div>p",1),"icon");
-    assert_eq!(expected,Statement::parse_input("(html>div>p)+icon"));
-
-    let expected = (("html","",1),"icon");
-    assert_eq!(expected,Statement::parse_input("(html)+icon"));
-
-    let expected = (("html","div>(p+div>p)",1),"icon");
-    assert_eq!(expected,Statement::parse_input("(html>div>(p+div>p))+icon"));
-
-    let expected = (("html.test.class","div:test:prop>(p+div>p)",3),"icon>p");
-    assert_eq!(expected,Statement::parse_input("(html.test.class>div:test:prop>(p+div>p))*3+icon>p"));
-    
-    let expected = (("html","(div>p)*3",1),"");
-    assert_eq!(expected,Statement::parse_input("html>(div>p)*3"));
+mod test_statement {
+  #[cfg(test)]
+  mod parser {
+      use crate::statement::Statement;
   
-    let expected = (("div","p",3),"");
-    assert_eq!(expected,Statement::parse_input("(div>p)*3"));
+    #[test]
+    fn should_correctly_parse_output() {
+      let expected = (("html","",1),"");
+      assert_eq!(expected,Statement::parse_input("html"));
+  
+      let expected = (("html","",3),"");
+      assert_eq!(expected,Statement::parse_input("html*3"));
+  
+      let expected = (("html","p",1),"");
+      assert_eq!(expected,Statement::parse_input("html>p"));
+  
+      let expected = (("html","",1),"p");
+      assert_eq!(expected,Statement::parse_input("html+p"));
+  
+      let expected = (("html","",1),"div>p+icon");
+      assert_eq!(expected,Statement::parse_input("html+div>p+icon"));
+  
+      let expected = (("html","div>p+icon",1),"");
+      assert_eq!(expected,Statement::parse_input("html>div>p+icon"));
+  
+      let expected = (("html","div>p",1),"icon");
+      assert_eq!(expected,Statement::parse_input("(html>div>p)+icon"));
+  
+      let expected = (("html","",1),"icon");
+      assert_eq!(expected,Statement::parse_input("(html)+icon"));
+  
+      let expected = (("html","div>(p+div>p)",1),"icon");
+      assert_eq!(expected,Statement::parse_input("(html>div>(p+div>p))+icon"));
+  
+      let expected = (("html.test.class","div:test:prop>(p+div>p)",3),"icon>p");
+      assert_eq!(expected,Statement::parse_input("(html.test.class>div:test:prop>(p+div>p))*3+icon>p"));
+      
+      let expected = (("html","(div>p)*3",1),"");
+      assert_eq!(expected,Statement::parse_input("html>(div>p)*3"));
+    
+      let expected = (("div","p",3),"");
+      assert_eq!(expected,Statement::parse_input("(div>p)*3"));
+    }
+  }
+
+  #[cfg(test)]
+  mod output {
+    use crate::statement::Statement;
+    
+    #[test]
+    fn should_return_html(){
+      assert_eq!("<html></html>".to_string(),Statement::new("html",0).parse())
+    }
+    
+    #[test]
+    fn should_combine_tag(){
+      assert_eq!("<html>\n\t<p></p>\n</html>".to_string(),Statement::new("html>p",0).parse());
+      assert_eq!("<html>\n\t<p></p>\n\t<a href=${1}></a>\n</html>".to_string(),Statement::new("html>p+a",0).parse());
+      
+      let expected = "<html>\n\t<div>\n\t\t<p></p>\n\t\t<div>\n\t\t\t<p></p>\n\t\t</div>\n\t</div>\n</html>".to_string();
+      assert_eq!(expected,Statement::new("html>div>p+div>p",0).parse());
+      
+      assert_eq!("<div>\n\t<p></p>\n</div>\n<p></p>".to_string(),Statement::new("(div>p)+p", 0).parse())
+    }
+  
+    #[test]
+    fn can_be_only_siblings(){
+      assert_eq!("<p></p>\n<div></div>".to_string(),Statement::new("p+div",0).parse());
+      assert_eq!("<p></p>\n<div></div>\n<p></p>".to_string(),Statement::new("p+div+p",0).parse())
+    }
+  
+    #[test]
+    fn can_handle_tag_group(){
+      let expected = "<html>\n\t<div>\n\t\t<p></p>\n\t</div>\n\t<div>\n\t\t<p></p>\n\t</div>\n</html>".to_string();
+      assert_eq!(expected,Statement::new("html>(div>p)+div>p",0).parse());
+    
+      let expected = "<html>\n\t<div>\n\t\t<div>\n\t\t\t<p></p>\n\t\t</div>\n\t\t<div>\n\t\t\t<p></p>\n\t\t</div>\n\t</div>\n\t<div>\n\t\t<p></p>\n\t</div>\n</html>".to_string();
+      assert_eq!(expected,Statement::new("html>(div>(div>p)+div>p)+div>p",0).parse());
+  
+      let expected = "<html>\n\t<div>\n\t\t<p></p>\n\t</div>\n\t<div>\n\t\t<div>\n\t\t\t<p></p>\n\t\t</div>\n\t\t<p></p>\n\t</div>\n</html>".to_string();
+      assert_eq!(expected,Statement::new("html>(div>p)+div>(div>p)+p",0).parse())
+    }
+  
+    #[test]
+    fn element_can_multiply(){
+      let expected = "<p></p>\n<p></p>\n<p></p>";
+      assert_eq!(expected.to_string(),Statement::new("p*3",0).parse());
+  
+      let expected = "<div>\n\t<p></p>\n\t<p></p>\n\t<p></p>\n</div>";
+      assert_eq!(expected.to_string(),Statement::new("div>p*3",0).parse());
+      
+      let expected = "<html>\n\t<div>\n\t\t<p></p>\n\t</div>\n\t<div>\n\t\t<p></p>\n\t</div>\n\t<div>\n\t\t<p></p>\n\t</div>\n</html>";
+      assert_eq!(expected.to_string(),Statement::new("html>(div>p)*3",0).parse())
+    }
+  
+    #[test]
+    fn single_element() {
+      // Can be single
+      let expected = "<html>\n\t<Icon/>\n</html>";
+      assert_eq!(expected.to_string(),Statement::new("html>Icon/",0).parse());
+      // Can't be Single if has child
+      let expected = "<html>\n\t<Icon>\n\t\t<p></p>\n\t</Icon>\n</html>";
+      assert_eq!(expected.to_string(),Statement::new("html>Icon/>p",0).parse())
+    }
+
+    #[test]
+    fn element_can_have_props() {
+      // Classes
+      let expected = "<div class=\"test echo bravo\"></div>";
+      assert_eq!(expected.to_string(),Statement::new("div.test.echo.bravo",0).parse());
+      // Single element Classes
+      let expected = "<div class=\"test echo bravo\"/>";
+      assert_eq!(expected.to_string(),Statement::new("div.test.echo.bravo/",0).parse());
+      // Props
+      let expected = "<Table header={title} name=\"my-table\"></Table>";
+      assert_eq!(expected.to_string(),Statement::new("Table:header={title}:name=my-table",0).parse());
+      // Single Element props
+      let expected = "<img src={image} alt=\"my own image\"/>";
+      assert_eq!(expected.to_string(),Statement::new("img:src={image}:alt=my own image/",0).parse());
+      // Props with dot
+      let expected = "<Table header={table.title} name=\"my-table\"></Table>";
+      assert_eq!(expected.to_string(),Statement::new("Table:header={table.title}:name=my-table",0).parse());
+      // Props is function
+      let expected = "<Table table={table.get()}></Table>";
+      assert_eq!(expected.to_string(),Statement::new("Table:table={table.get()}",0).parse());
+      // Destructured
+      let expected = "<Table {...props}></Table>";
+      assert_eq!(expected.to_string(),Statement::new("Table:{...props}",0).parse());
+      // Text
+      let expected = "<div>My test text is awesome</div>";
+      assert_eq!(expected.to_string(),Statement::new("div<My test text is awesome",0).parse());
+      // All
+      let expected = "<div class=\"test\" data={myData}>My test text is awesome</div>";
+      assert_eq!(expected.to_string(),Statement::new("div.test:data={myData}<My test text is awesome",0).parse());
+    }
+  
+    #[test]
+    fn should_handle_complex_query(){
+      let expected = "<div>\n\t<div>\n\t\t<div>\n\t\t\t<p></p>\n\t\t</div>\n\t\t<p></p>\n\t</div>\n\t<p></p>\n</div>";
+  
+      assert_eq!(expected.to_string(),Statement::new("div>(div>(div>p)+p)+p",0).parse());
+      
+      let expected = "<div class=\"fixed bottom-0 left-0 right-0 top-0 z-20 flex animate-appear flex-col items-center justify-center bg-black/70 backdrop-blur-md\" onClick$={onHide$}>\n\t<div class=\"flex flex-col items-center justify-center p-20\">\n\t\t<div class=\"delay animate-appear opacity-0\">\n\t\t\t<Logo class=\"size-40\" fill=\"white\"/>\n\t\t</div>\n\t\t<div>\n\t\t\t<Form class=\"mx-2 flex animate-slideB flex-col items-center gap-2 rounded-lg bg-surface p-5\" action={login}>\n\t\t\t\t<div class=\"flex items-center gap-4\">\n\t\t\t\t\t<p class=\"text-3xl font-medium\">Connexion</p>\n\t\t\t\t\t<Icon icon=\"lock\"/>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"flex flex-col gap-2 self-stretch px-3\">\n\t\t\t\t\t<Textfield label=\"Email\" type=\"email\" required name=\"email\" placeholder=\"j.dupont@example.com\" icon=\"atSign\"/>\n\t\t\t\t\t<Textfield label=\"Password\" type=\"password\" required name=\"password\" placeholder=\"Votre mot de passe\" icon=\"lock\"/>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"flex flex-col gap-2\">\n\t\t\t\t\t<Button color=\"current\" type=\"submit\">Connexion</Button>\n\t\t\t\t\t<Link href=\"#\" class=\"text-secondary transition-colors\">Mot de passe oublié ?</Link>\n\t\t\t\t</div>\n\t\t\t</Form>\n\t\t</div>\n\t</div>\n</div>";
+      assert_eq!(expected.to_string(),Statement::new("div.fixed.bottom-0.left-0.right-0.top-0.z-20.flex.animate-appear.flex-col.items-center.justify-center.bg-black/70.backdrop-blur-md:onClick$={onHide$}>div.flex.flex-col.items-center.justify-center.p-20>(div.delay.animate-appear.opacity-0>Logo.size-40:fill=white/)+div>Form.mx-2.flex.animate-slideB.flex-col.items-center.gap-2.rounded-lg.bg-surface.p-5:action={login}>(div.flex.items-center.gap-4>p.text-3xl.font-medium<Connexion+Icon:icon=lock/)+(div.flex.flex-col.gap-2.self-stretch.px-3>Textfield:label=Email:type=email:required;:name=email:placeholder=j.dupont@example.com:icon=atSign/+Textfield:label=Password:type=password:required;:name=password:placeholder=Votre mot de passe:icon=lock/)+div.flex.flex-col.gap-2>Button:color=current:type=submit<Connexion+Link:href=#.text-secondary.transition-colors<Mot de passe oublié ?",0).parse())
+    }
+  
+    #[test]
+    fn should_handle_snippet(){
+      let expected = "<a href=${1}></a>";
+      assert_eq!(expected.to_string(),Statement::new("a",0).parse());
+  
+      let expected = "<>\n\t<label for=${1}></label>\n\t<input type=\"checkbox\" name=${1} id=${1}/>\n</>";
+      assert_eq!(expected.to_string(),Statement::new("e>label+input:c",0).parse());
+      assert_eq!(expected.to_string(),Statement::new(">label+input:c",0).parse());
+  
+      let expected = "<div class=\"foo bar\"></div>";
+      assert_eq!(expected.to_string(),Statement::new(".foo.bar",0).parse());
+    }
   }
 }
+
+
