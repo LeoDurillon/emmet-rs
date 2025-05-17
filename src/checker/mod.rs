c5216287b1f0095b1c243df49ae5371a0122e12a
LeoDurillon <durillon.leo@outlook.com>
2025-05-17 20:12:34 +0200

    feat: check input with regex

--- src/checker/mod.rs
+++ src/checker/mod.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use regex::Regex;
+
 pub fn input_correctly_close(input:&str) -> bool {
   let mut opener:Vec<char>=Vec::new();
 
@@ -30,12 +32,19 @@ pub fn input_correctly_close(input:&str) -> bool {
   return opener.len() == 0;
 }
 
+pub fn input_correctly_structured(input:&str) -> bool {
+  let regex:Regex = Regex::new(r"^(?:\(?[>+]?[a-zA-Z]+[.>:+]?(?:=(?:\{[a-zA-Z]+\}|[a-zA-Z]+))?\(?(?:<[\w ]+)?\)*(?:\*\d+)?)+$").unwrap();
+
+  regex.is_match(input)
+}
+
+
 #[cfg(test)]
 mod checker_test {
-    use crate::checker::input_correctly_close;
+    use crate::checker::{input_correctly_close, input_correctly_structured};
 
   #[test]
-  fn should_check_input(){
+  fn should_check_closing(){
     assert_eq!(true,input_correctly_close("html"));
 
     assert_eq!(true,input_correctly_close("html>p"));
@@ -57,5 +66,32 @@ mod checker_test {
     assert_eq!(false,input_correctly_close("div:foo={bar>p"));
 
     assert_eq!(false,input_correctly_close("div+("));
+  }
+
+  #[test]
+  fn should_check_structure(){
+    assert_eq!(false,input_correctly_structured("return function() "));
+
+    assert_eq!(true,input_correctly_structured("html>p"));
+
+    assert_eq!(true,input_correctly_structured("(html>div>p)+icon"));
+
+    assert_eq!(true,input_correctly_structured("(html)+icon"));
+
+    assert_eq!(true,input_correctly_structured("(html>div>(p+div>p))+icon"));
+
+    assert_eq!(true,input_correctly_structured("(html.test.class>div:test:prop>(p+div>p))*3+icon>p"));
+
+    assert_eq!(true,input_correctly_structured("html>(div>p)*3"));
+
+    assert_eq!(true,input_correctly_structured("(div>p)*3"));
+
+    assert_eq!(true,input_correctly_structured("(div>p"));
+
+    assert_eq!(false,input_correctly_structured("div:foo={bar>p"));
+
+    assert_eq!(true,input_correctly_structured("div+("));
+
+    assert_eq!(false,input_correctly_structured("div > p"));
   }
 }
